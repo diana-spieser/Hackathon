ad3b11950d2758e8215e7cb870fb22b2c45e60f5
Jonathan Leal <jonathanleal@msn.com>
2023-01-21 04:35:05 +0100

    Home page banner + card sections

---
+++ js/home_page.js
@@ -0,0 +1,12 @@
+
+$( document ).ready(function() {
+
+var options = {
+  strings: ['Include me !!!'],
+  typeSpeed: 70,
+  loop:true
+};
+
+var typed = new Typed('.element', options);
+console.log('passed')
+});
