896ce558f4b934d79ee06b3e0064e400006be6bd
Ekter <ektermoi1@gmail.com>
2023-01-21 10:45:40 +0100

    func : redirect properly from create to dashboard

--- js/create.js
+++ js/create.js
@@ -9,4 +9,15 @@ $(document).ready(function() {
     $('#textarea_title').val(title);
     // Set the location
     $('#textarea_content').val(location);
+    // edit button_create click
+
+    button = $('#button_create');
+    button.click(function() {
+    // Get the title
+    var title = $('#textarea_title').val();
+    // Get the location
+    var location = $('#textarea_content').val();
+    // Redirect to the create page with the title and location
+    window.location.href = 'dashboard.html?title=' + title + '&location=' + location;
+});
 });
